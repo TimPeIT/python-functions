a8d922b8ba141c41cafbc51d22d138c463f1182c
TimPeIT <timpella95@web.de>
2025-03-28 13:40:22 +0100

    Schoolwork done

--- user-profile-functions.py
+++ user-profile-functions.py
@@ -1,14 +1,18 @@
-def create_user_profile(**kwargs):
-    profile = {}
+def create_user_profile(name, email, **kwargs):
+    profile = {
+        'name': name,
+        'email': email
+    }
 
     for key, value in kwargs.items():
         profile[key] = value
 
+
     return profile
     
 
-alice = create_user_profile(name="Alice", age=33, city="New York")
-bob = create_user_profile(name="Bob", last_name="Smith", age=22, phone_number=123456789)
+alice = create_user_profile(name="Alice", email="alice@example.de", age=33, city="New York")
+bob = create_user_profile(name="Bob", email="bob@example.de", last_name="Smith", age=22, phone_number=123456789)
 
 print(alice)
 print(bob)
